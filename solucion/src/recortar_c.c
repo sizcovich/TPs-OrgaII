285b5375a42c58c848258ccc5a169f10086df980
sizcovich <sizcovich@b194edf2-a248-48e4-bed8-4280180ada1a>
2013-04-24 18:10:21 +0000

    Recortar.c funcionando 10 ptos

--- solucion/src/recortar_c.c
+++ solucion/src/recortar_c.c
@@ -12,41 +12,37 @@ void recortar_c (
 
 	int i, j;
 	//i es la fila, j es la columna
-	int coordA[2] = {0};
+/*	int coordA[2] = {0};
 	int coordB[2] = {0, w-tam};
 	int coordC[2] = {h-tam, 0};
 	int coordD[2] = {h-tam, w-tam};
 	
-	/*Recorre desde el 0,0 hasta tam,tam la matriz fuente y la pega en
+	Recorre desde el 0,0 hasta tam,tam la matriz fuente y la pega en
 	 destino en el lugar correspondiente teniendo en cuenta el desplazamiento*/
-	for (j = 0; i < tam; ++j) {
-		for (i = 0; j < tam; ++i) {
-			int desplazado = (j+tam)+((i*dst_row_size)+(dst_row_size*tam));
-			dst[desplazado] = src[j + coordA[1] + i*src_row_size + coordA[0]*src_row_size];
+	for (j = 0; j < tam; ++j) {
+		for (i = 0; i < tam; ++i) {
+			dst_matrix[i+tam][j+tam] = src_matrix[i][j];
 		}
 	}
 	/*Recorre desde el 0,w-tam hasta tam,w la matriz fuente y la pega en
 	 destino en el lugar correspondiente teniendo en cuenta el desplazamiento*/
-	for (j = 0; i < tam; ++j) {
-		for (i = 0; j < tam; ++i) {
-			int desplazado = j + ((i*dst_row_size)+(dst_row_size*tam));
-			dst[desplazado] = src[j + coordB[1] + i*src_row_size + coordB[0]*src_row_size];
+	for (j = 0; j < tam; ++j) {
+		for (i = 0; i < tam; ++i) {
+			dst_matrix[i+tam][j] = src_matrix[i][w-tam+j];
 		}
 	}
 	/*Recorre desde el h-tam,0 hasta h,tam la matriz fuente y la pega en
 	 destino en el lugar correspondiente teniendo en cuenta el desplazamiento*/
-	for (j = 0; i < tam; ++j) {
-		for (i = 0; j < tam; ++i) {
-			int desplazado = (j+tam)+(i*dst_row_size);
-			dst[desplazado] = src[j + coordC[1] + i*src_row_size + coordC[0]*src_row_size];
+	for (j = 0; j < tam; ++j) {
+		for (i = 0; i < tam; ++i) {
+			dst_matrix[i][tam+j] = src_matrix[i+h-tam][j];
 		}
 	}
 	/*Recorre desde el h-tam,w-tam hasta h,w la matriz fuente y la pega en
 	 destino en el lugar correspondiente teniendo en cuenta el desplazamiento*/
-	for (j = 0; i < tam; ++j) {
-		for (i = 0; j < tam; ++i) {
-			int desplazado = j+(i*dst_row_size);
-			dst[desplazado] = src[j + coordD[1] + i*src_row_size + coordD[0]*src_row_size];
+	for (j = 0; j < tam; ++j) {
+		for (i = 0; i < tam; ++i) {
+			dst_matrix[i][j] = src_matrix[i+h-tam][j+w-tam];
 		}
 	}
 }
