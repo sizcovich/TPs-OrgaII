aa4251f58c44ac0f44112cf00b44215159938cc4
Sabrina <sizcovich@users.noreply.github.com>
2013-04-24 19:35:44 +0000

    funciones umbralizar y colorizar 10 points

--- solucion/src/colorizar_c.c
+++ solucion/src/colorizar_c.c
@@ -1,12 +1,79 @@
 void colorizar_c (
 	unsigned char *src,
 	unsigned char *dst,
-	int m,
-	int n,
+	int h,
+	int w,
 	int src_row_size,
 	int dst_row_size,
 	float alpha
 ) {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
+	//1er Byte = B, 2do = G, 3ero = R
+	int i, j, l, k, maximo;
+	for(i=1; i<h; ++i){
+		for (j=3; j < 3*w; j+=3){
+			int maximoR, maximoB, maximoG = 0;
+			for(k=-1; k<1; ++k){
+				for (l=-3; l < 3; l+=3){	
+					if(maximoR < src_matrix[i+k][j+2+l]){
+						maximoR = src_matrix[i+k][j+2+l];
+					}
+				}
+	
+			}
+			for(k=-1; k<1; ++k){
+				for (l=-3; l < 3; l+=3){	
+					if(maximoG < src_matrix[i+k][j+1+l]){
+						maximoG = src_matrix[i+k][j+1+l];
+					}
+		
+				}
+	
+			}
+			for(k=-1; k<1; ++k){
+				for (l=-3; l < 3; l+=3){	
+					if(maximoB < src_matrix[i+k][j+0+l]){
+						maximoB = src_matrix[i+k][j+0+l];
+					}
+		
+				}
+	
+			}//Llamamos 0 a B, 1 a G y 2 a R 
+			if(maximoB <= maximoR){
+				if (maximoG > maximoR) {
+					maximo = 1;
+				} else {
+					maximo = 2;
+				}
+			}else{
+				if (maximoG > maximoB) {
+					maximo = 1;
+				} else {
+					maximo = 0;
+				}
+			}
+			float fcR, fcB, fcG;
+			switch (maximo){
+				case 0:
+					fcB = (1+alpha);
+					fcG = (1-alpha);
+					fcR = (1-alpha);
+					break;
+				case 1:
+					fcB = (1-alpha);
+					fcG = (1+alpha);
+					fcR = (1-alpha);
+					break;
+				case 2:
+					fcB = (1-alpha);
+					fcG = (1-alpha);
+					fcR = (1+alpha);
+					break;
+			}
+			dst_matrix[i][j] = fcB*src_matrix[i][j];
+			dst_matrix[i][j+1] = fcG*src_matrix[i][j+1];
+			dst_matrix[i][j+2] = fcR*src_matrix[i][j+2];
+		}
+	}
 }
--- solucion/src/umbralizar_c.c
+++ solucion/src/umbralizar_c.c
@@ -3,8 +3,8 @@
 void umbralizar_c (
 	unsigned char *src,
 	unsigned char *dst,
-	int m,
-	int n,
+	int h,
+	int w,
 	int row_size,
 	unsigned char min,
 	unsigned char max,
@@ -12,4 +12,16 @@ void umbralizar_c (
 ) {
 	unsigned char (*src_matrix)[row_size] = (unsigned char (*)[row_size]) src;
 	unsigned char (*dst_matrix)[row_size] = (unsigned char (*)[row_size]) dst;
+	int i, j;
+	for (i=0; i<h; ++i){
+		for(j=0; j<w; j++){
+			if(src_matrix[i][j] < min){
+				dst_matrix[i][j] = 0;
+			} else if (src_matrix[i][j] > max){
+				dst_matrix[i][j] = 255;
+			} else{
+				dst_matrix[i][j] = (src_matrix[i][j]/q)*q;
+			}
+		}
+	}
 }
