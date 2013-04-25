4395c1553aa08bf614b939a3604843c26dfcc2db
Sabrina <sizcovich@users.noreply.github.com>
2013-04-25 19:41:07 +0000

    arregle una cosa pero sigue sin funcionar el rotar.c

--- solucion/src/rotar_c.c
+++ solucion/src/rotar_c.c
@@ -9,14 +9,13 @@ void rotar_c (
 ) {
 	unsigned char (*src_matrix)[src_row_size] = (unsigned char (*)[src_row_size]) src;
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
-	int i,j;
+	int i,j,u,v;
 	int cx = w/2;
 	int cy = h/2;
-	int u, v;
-	for (i=0; i < h; i+=2){
-		for (j=0; j < w; j+=2){
-			u = cx + (sqrt(2)/2)*(i-cx) - (sqrt(2)/2)*(j-cy);
-			v = cy + (sqrt(2)/2)*(i-cx) - (sqrt(2)/2)*(j-cy);
+	for (i=0; i < h; ++i){
+		for (j=0; j < w; ++j){
+			u = cx + ((sqrt(2)/2)*(i-cx)) - ((sqrt(2)/2)*(j-cy));
+			v = cy + ((sqrt(2)/2)*(i-cx)) - ((sqrt(2)/2)*(j-cy));
 			if ((0<=u && u<=w) && (0<=v && v<=h)){
 				dst_matrix[i][j] = src_matrix[u][v];
 			}else{
