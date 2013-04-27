ecfbcc563c5d0fb51e1fca952bd3ef27c0c239df
Sabrina <sizcovich@users.noreply.github.com>
2013-04-27 15:55:19 +0000

    iniciamos 1er fc en asm

--- solucion/src/recortar_asm.asm
+++ solucion/src/recortar_asm.asm
@@ -1,8 +1,8 @@
 ; void recortar_asm (
 ; 	unsigned char *src,
 ; 	unsigned char *dst,
-; 	int m,
-; 	int n,
+; 	int h,
+; 	int w,
 ; 	int src_row_size,
 ; 	int dst_row_size,
 ; 	int tam
@@ -11,8 +11,8 @@
 ; Parámetros:
 ; 	rdi = src
 ; 	rsi = dst
-; 	rdx = m
-; 	rcx = n
+; 	rdx = h
+; 	rcx = w
 ; 	r8 = src_row_size
 ; 	r9 = dst_row_size
 ; 	rbp + 16 = tam
@@ -24,19 +24,36 @@ global recortar_asm
 section .text
 
 recortar_asm:
-	;; TODO: Implementar
 
 	push rbp
 	mov rbp, rsp
+    push r12
+    push r13
 
-	sub rsp, 8
+    mov r12, rdx
+    mov r13, rcx
 
-	push qword [rbp + 16]
-	call recortar_c
-	add rsp, 8
+;Cuadrante B
+;--------------
+;rdx es la fila
+;rcx es la columna
 
-	add rsp, 8
+    xor rdx, rdx    ;primer fila
 
-	pop rbp
+	xor r10, r10
+	mov r10d, r13d	;Copio el ancho
+	sub r10d, [rbp+16]	;Le resto el tam para obtener el offset
+
+.cuadradoB:
+	mov rcx, r10	;Me paro al inicio del cuadrado
+	
+
+;	push qword [rbp + 16]
+;	call recortar_c
+;	add rsp, 8
+
+    pop r13
+    pop r12
+    pop rbp
 
 	ret
