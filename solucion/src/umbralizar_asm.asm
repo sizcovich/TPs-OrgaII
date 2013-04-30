159440ad9cb385c9ee2f7c62ec7e94aab5d9e49b
Sabrina <sizcovich@users.noreply.github.com>
2013-04-30 23:59:55 +0000

    ok

--- solucion/src/umbralizar_asm.asm
+++ solucion/src/umbralizar_asm.asm
@@ -26,34 +26,34 @@ global umbralizar_asm
 section .text
 
 umbralizar_asm:
-	push rdx
-	push rbp
-	xor rdx, rdx
-	
-	cmp srcij, r9
-	jb .cero
-	cmp srcij, [rbp+16]
-	ja .maximo
-	mov rdx, srcij
-	div rdx, [rbp+24]
-	imul rdx, [rbp+24]
-	mov dstij, srcij	
-	push rbp
-	mov rbp, rsp
+;	push rdx
+;	push rbp
+;	xor rdx, rdx
+;	
+;	cmp srcij, r9
+;	jb .cero
+;	cmp srcij, [rbp+16]
+;	ja .maximo
+;	mov rdx, srcij
+;	div rdx, [rbp+24]
+;	imul rdx, [rbp+24]
+;	mov dstij, srcij	
+;	push rbp
+;	mov rbp, rsp
 
 ;	push qword [rbp + 24]
 ;	push qword [rbp + 16]
 ;	call umbralizar_c
 ;	add rsp, 16
-.fin
-	pop rbp
-	pop rdx
-	ret
-
-.cero: 
-	dstij = 0x00
-	jmp .fin
-	
-.maximo:
-	dstij = 0xFF
-	jmp .fin
+;.fin
+;	pop rbp
+;	pop rdx
+;	ret
+;
+;.cero: 
+;	dstij = 0x00
+;	jmp .fin
+;	
+;.maximo:
+;	dstij = 0xFF
+;	jmp .fin
