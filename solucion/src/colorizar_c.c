d0cf5ae5146efdf07f3b1afdd6fb5fe148ce0129
codingtornado <codingTornado@users.noreply.github.com>
2013-05-08 03:06:43 +0000

    Los cambios a colorizar_c que me olvide de commitear antes

--- solucion/src/colorizar_c.c
+++ solucion/src/colorizar_c.c
@@ -11,48 +11,41 @@ void colorizar_c (
 	unsigned char (*dst_matrix)[dst_row_size] = (unsigned char (*)[dst_row_size]) dst;
 	//1er Byte = B, 2do = G, 3ero = R
 	int i, j, l, k, maximo;
+	
 	for(i=1; i<h; ++i){
 		for (j=3; j < 3*w; j+=3){
 			int maximoR, maximoB, maximoG = 0;
-			for(k=-1; k<1; ++k){
+			
+			for(k=-1; k<1; ++k){	//Busco el maximoR
 				for (l=-3; l < 3; l+=3){	
 					if(maximoR < src_matrix[i+k][j+2+l]){
 						maximoR = src_matrix[i+k][j+2+l];
 					}
 				}
-	
 			}
-			for(k=-1; k<1; ++k){
+			for(k=-1; k<1; ++k){	//Busco el maximoG
 				for (l=-3; l < 3; l+=3){	
 					if(maximoG < src_matrix[i+k][j+1+l]){
 						maximoG = src_matrix[i+k][j+1+l];
 					}
-		
 				}
-	
 			}
-			for(k=-1; k<1; ++k){
+			for(k=-1; k<1; ++k){	//Busco el maximoB
 				for (l=-3; l < 3; l+=3){	
 					if(maximoB < src_matrix[i+k][j+0+l]){
 						maximoB = src_matrix[i+k][j+0+l];
 					}
-		
-				}
-	
-			}//Llamamos 0 a B, 1 a G y 2 a R 
-			if(maximoB <= maximoR){
-				if (maximoG > maximoR) {
-					maximo = 1;
-				} else {
-					maximo = 2;
-				}
-			}else{
-				if (maximoG > maximoB) {
-					maximo = 1;
-				} else {
-					maximo = 0;
 				}
 			}
+
+			//Llamamos 0 a B, 1 a G y 2 a R 
+			if(maximoR >= maximoG && maximoR >= maximoB)
+				maximo = 2;
+			if(maximoR < maximoG && maximoG >= maximoB)
+				maximo = 1;
+			if(maximoR < maximoB && maximoR < maximoB)
+				maximo = 0;
+
 			float fcR, fcB, fcG;
 			switch (maximo){
 				case 0:
@@ -71,9 +64,26 @@ void colorizar_c (
 					fcR = (1+alpha);
 					break;
 			}
-			dst_matrix[i][j] = fcB*src_matrix[i][j];
-			dst_matrix[i][j+1] = fcG*src_matrix[i][j+1];
-			dst_matrix[i][j+2] = fcR*src_matrix[i][j+2];
+
+			int val = fcB*src_matrix[i][j];
+			if(val < 255){
+				dst_matrix[i][j] = val;
+			} else {
+				dst_matrix[i][j] = 255;
+			}
+			val = fcG*src_matrix[i][j+1];
+			if(val < 255){
+				dst_matrix[i][j+1] = val;
+			} else {
+				dst_matrix[i][j+1] = 255;
+			}
+			val = fcR*src_matrix[i][j+2];
+			if(val < 255){
+				dst_matrix[i][j+2] = val;
+			} else {
+				dst_matrix[i][j+2] = 255;
+			}
+			
 		}
 	}
 }
