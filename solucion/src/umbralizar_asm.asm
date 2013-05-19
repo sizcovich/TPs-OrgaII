35b96a12f20f1de9ff41acecf28da144e79d8163
Sabrina <sizcovich@users.noreply.github.com>
2013-05-19 23:19:19 +0000

    umbralizar

--- solucion/src/umbralizar_asm.asm
+++ solucion/src/umbralizar_asm.asm
@@ -1,3 +1,4 @@
+BITS 64
 ; void umbralizar_c (
 ; 	unsigned char *src,
 ; 	unsigned char *dst,
@@ -12,8 +13,8 @@
 ; Parámetros:
 ; 	rdi = src
 ; 	rsi = dst
-; 	rdx = m
-; 	rcx = n
+; 	rdx = m = h
+; 	rcx = n = w
 ; 	r8 = row_size
 ; 	r9 = min
 ; 	rbp + 16 = max
@@ -26,34 +27,79 @@ global umbralizar_asm
 section .text
 
 umbralizar_asm:
-;	push rdx
-;	push rbp
-;	xor rdx, rdx
-;	
-;	cmp srcij, r9
-;	jb .cero
-;	cmp srcij, [rbp+16]
-;	ja .maximo
-;	mov rdx, srcij
-;	div rdx, [rbp+24]
-;	imul rdx, [rbp+24]
-;	mov dstij, srcij	
-;	push rbp
-;	mov rbp, rsp
+	push rbp
+	mov rbp, rsp
+	;-----voy a calcular el tamaño
+    mov rcx, r8
+	imul rcx, rdx ;me guardo el tamaño
+	;-----me guardo los parametros
+	;me guardo los parámetros de entrada necesarios
+	mov xmm5, [rbp+24]
+	pshufd xmm5, xmm5, 0 ;Q en XMM5
+	movdqu xmm7, xmm5 ;me guardo Q en float
+	cvtdq2ps xmm7, xmm7
+	movdqu xmm0, r9
+	pshufb xmm0, 0 ;me guardo el minimo en xmm0
+	movdqu xmm1, [rbp+16]
+	pshufb xmm1, 0 ;me guardo el maximo en xmm1
+.ciclo:
+	movdqu xmm2, [rdi]
+	;-----empiezo a comparar
+	movups xmm3, xmm0 ;me guardo el minimo
+	pcmpgtw xmm3, xmm2 ;min > pixel, voy a ver cuando es 0
+	;-----quiero ver cuando el pixel es menor al minimo
+	movups xmm4, xmm1 ;max > pixel
+	pcmpgtw xmm4, xmm2
+	;-----me guardo la mascara de maximo
+	pxor xmm15, xmm15
+    movdqu xmm6, xmm2
+	punpckhbw xmm6, xmm15 ;en xmm6 tengo la parte alta
+	punpcklbw xmm2, xmm15 ;en xmm2 tengo la parte baja
+	movdqu xmm10, xmm6
+	movdqu xmm11, xmm2
+	punpckhwd xmm10, xmm15 ;desempaqueto
+	punpcklwd xmm11, xmm15
+	punpcklwd xmm6, xmm15
+	punpckhwd xmm2, xmm15
+	;-----convertimos los int a float
+	cvtdq2ps xmm10, xmm10
+	cvtdq2ps xmm6, xmm6
+	cvtdq2ps xmm2, xmm2
+	cvtdq2ps xmm11, xmm11
+	;-----Divido por Q
+	divps xmm11, xmm7
+	divps xmm2, xmm7
+	divps xmm6, xmm7
+	divps xmm10, xmm7
+	;-----multiplico por Q
+	roundps xmm10, xmm10, 11B
+	roundps xmm6,xmm6, 11B
+	roundps xmm2,xmm2, 11B
+	roundps xmm11,xmm11, 11B
+	;-----multiplico por Q
+	mulps xmm6, xmm7
+	mulps xmm2, xmm7
+	mulps xmm11, xmm7
+	mulps xmm10, xmm7
+	;----vuelvo a pasar a doubleword
+	cvtps2dq xmm6, xmm6
+	cvtps2dq xmm10, xmm10
+	cvtps2dq xmm11, xmm11
+	cvtps2dq xmm2, xmm2
+	;----empaqueto
+	packusdw xmm10, xmm6
+	packusdw xmm2, xmm11
+	packusdw xmm10, xmm2
+	;----filtrado por mascara
+	pand xmm10, xmm3 ;filtre por el minimo
+	por xmm10, xmm4 ;filtre por el maximo
+	movdqu [rsi], xmm10 ;lo guardo en destino
+	sub rcx, 15
+	add rdi, 16
+	add rsi, 16
+	loop .ciclo
+.fin:
+	pop rbp
+	ret
 
-;	push qword [rbp + 24]
-;	push qword [rbp + 16]
-;	call umbralizar_c
-;	add rsp, 16
-;.fin
-;	pop rbp
-;	pop rdx
-;	ret
-;
-;.cero: 
-;	dstij = 0x00
-;	jmp .fin
-;	
-;.maximo:
-;	dstij = 0xFF
-;	jmp .fin
+
