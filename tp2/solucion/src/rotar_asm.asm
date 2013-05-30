bbe719c52ebc0674f1ce17cacf85101dc6470007
sizcovich <sizcovich@b194edf2-a248-48e4-bed8-4280180ada1a>
2013-05-30 19:35:50 +0000

    algunas cosas de rotar

--- tp2/solucion/src/rotar_asm.asm
+++ tp2/solucion/src/rotar_asm.asm
@@ -32,6 +32,11 @@ rotar_asm:
 	push RBX
 	sub rsp, 8
 	
+	;Idst = Isrc(u,v) entre 0<=u<Isrc_width y 0<=v<Isrc_height
+	;u  = cx + sqrt(2)/2 (x-cx) - sqrt(2)/2 (y-cy)
+	;v = cy + sqrt(2)/2 (x-cx) + sqrt(2)/2 (y-cy)
+	;cx = [Isrc_width/2]
+	;cy = [Isrc_height/2]
 	mov r12, rdx ;tengo a h
 	mov r14, rcx ;tengo a w
 	mov rax, rdx
@@ -42,21 +47,19 @@ rotar_asm:
 	mov r15, rax ;Cx
 	;en r8 y en r9 voy a guardar u y v
 	
-;empiezo a comparar
-	;aca recorro la matriz
 	
 	;aca veo u
 	cmp rsi, 0
-	jl .noentra
+	jl .noEntra
 	cmp rsi, r14
-	jge .noentra
+	jge .noEntra
 	;aca veo v
 	cmp rdi, 0
-	jl .noentra
+	jl .noEntra
 	cmp rdi, r14
-	jge .noentra
+	jge .noEntra
 ;no salto entonces hay que operar
-.esmenor:
+.esMenor:
 	mov r8, r15
 	mov r10, 2
 	sqrtps r10, r10
@@ -68,14 +71,38 @@ rotar_asm:
 	mov rax, r15
 	mul r10
 	
-.noentra:
+.noEntra:
 	mov rsi, 0
-	
-	;Idst = Isrc(u,v) entre 0<=u<Isrc_width y 0<=v<Isrc_height
-	;u  = cx + sqrt(2)/2 (x-cx) - sqrt(2)/2 (y-cy)
-	;v = cy + sqrt(2)/2 (x-cx) + sqrt(2)/2 (y-cy)
-	;cx = [Isrc_width/2]
-	;cy = [Isrc_height/2]
+
+		
+	;empiezo a comparar
+	;aca recorro la matriz
+.ciclo:
+	xor r15, r15    ;Acumulador
+	add r15, 16
+	mov rcx, r10	;Me paro al inicio del cuadrado
+	add rcx, rdx	;Me muevo al principio del cuadrado en la linea rdx
+.loopLinea:
+	movdqu xmm0, [rdi+rcx]	;Tomo el pedazo de memoria
+	movdqu [rsi+rbx], xmm0	;Guardo los valores en destino
+	add rcx, 16
+	add rbx, 16
+	add r15, 16
+  
+	cmp r15d, eax	;Me fijo que el desplazamiento horizontal todavia este dentro de la linea
+	jl .loopLinea
+	sub r15d, eax	;Me quedo con la diferencia por la que me pase
+	sub ecx, r15d	;Retrocedo el exceso
+	sub ebx, r15d
+	movdqu xmm0, [rdi+rcx]	;Tomo el pedazo de memoria
+	movdqu [rsi+rbx], xmm0	;Guardo los valores en destino
+
+	add rdx, r8
+	add r11, r9
+	mov rbx, r11
+	inc r14
+	cmp r14d, eax	;Me fijo que el acumulador sea menor que el total de filas
+	jl .ciclo
 	
 	
 ;*******************
