535186f06c2a2c6cc95fd56e4ae08148220ef9e2
sizcovich <sizcovich@b194edf2-a248-48e4-bed8-4280180ada1a>
2013-06-02 23:39:48 +0000

    halftone

--- tp2/solucion/src/halftone_asm.asm
+++ tp2/solucion/src/halftone_asm.asm
@@ -19,6 +19,16 @@ extern halftone_c
 
 global halftone_asm
 
+section .rodata
+
+dosCientosCinco: DW 205, 205, 205, 205, 205, 205, 205, 205
+cuatroCientosDiez: DW 410, 410, 410, 410, 410, 410, 410, 410
+seisCientosQuince: DW 615, 615, 615, 615, 615, 615, 615, 615
+ochoCientosVeinte: DW 820, 820, 820, 820, 820, 820, 820, 820
+inversor: DQ 0xffffffffffffffff,0xffffffffffffffff
+filaAbajo: DQ 0x00ff00ff00ff00ff,0x00ff00ff00ff00ff
+filaArriba: DQ 0xff00ff00ff00ff00,0xff00ff00ff00ff00
+
 section .text
 
 halftone_asm:
@@ -26,24 +36,104 @@ halftone_asm:
 	mov RBP, RSP
 	push R12
 	push R13
-	
-;	mov r12, rdx ;me guardo el alto
-;	mov r13, rcx ;me guardo el ancho
+	push r14
+	push r15
+	push rbx
+	sub rsp, 8
+
+	mov r15, rdi
+	;voy a ver si el ancho y largo es par
+	mov r12, rdx ;me guardo el alto
+	mov r13, rcx ;me guardo el ancho
 	;veo si tiene ancho y largo pares
-;	mov rax, r12
-;.paridad:
-;	cmp rax, 0
-	jl .resto
-;	div rdi, 2
+	mov rbx, 2
+	mov rax, r12
+	div rbx
+	cmp rdx, 0
+	je .verAncho
+	dec r12
+.verAncho:
+	mov rax, r13
+	div rbx
+	cmp rdx, 0
+	je .inicio
+	dec r13
+.inicio:
+    xor rbx, rbx
 
-.resto:
-	sub r12, 1
-	
-;*******************************
+	movdqu xmm15, [dosCientosCinco]
+	movdqu xmm14, [cuatroCientosDiez]
+	movdqu xmm13, [seisCientosQuince]
+	movdqu xmm12, [ochoCientosVeinte]
+	movdqu xmm11, [inversor]
+	movdqu xmm10, [filaAbajo]
+	movdqu xmm9, [filaArriba]
+.ciclo:
+	add r15, r8
+	movdqu xmm0, [rdi+rbx]
+	movdqu xmm2, [r15+rbx]
 
-	call halftone_c
+	xor xmm8, xmm8
+	movdqu xmm1, xmm0
+	movdqu xmm3, xmm2
+	punpckhbw xmm0, xmm8
+	punpcklbw xmm3, xmm8
+	punpckhbw xmm2, xmm8
+	punpcklbw xmm1, xmm8
 
-;*******************************	
+	;sumamos para obtener los valores de los cuadraditos
+	phaddw xmm0, xmm1
+	phaddw xmm2, xmm3
+	paddw xmm0, xmm2
+
+	movdqu xmm1, xmm0
+	movdqu xmm2, xmm0
+	movdqu xmm3, xmm0
+	movdqu xmm4, xmm0
+	movdqu xmm5, xmm0
+	movdqu xmm6, xmm0
+	movdqu xmm7, xmm0
+
+	pcmpgtw xmm0, xmm15	;obtenemos los valores mayores a 205
+	pcmpeqw xmm1, xmm15	;Obtenemos los valores iguales a 205
+	por xmm0, xmm1
+
+	pcmpgtw xmm2, xmm14	;obtenemos los valores mayores a 410
+	pcmpeqw xmm3, xmm14	;Obtenemos los valores iguales a 410
+	por xmm2, xmm3
+
+	pcmpgtw xmm4, xmm13	;obtenemos los valores mayores a 615
+	pcmpeqw xmm5, xmm13	;Obtenemos los valores iguales a 615
+	por xmm4, xmm5
+
+	pcmpgtw xmm6, xmm12	;obtenemos los valores mayores a 820
+	pcmpeqw xmm7, xmm12	;Obtenemos los valores iguales a 820
+	por xmm6, xmm7
+
+	;En caso de que sea >= 205 solo cambio el valor de arriba
+	;En caso de que sea >= 410 cambio el de abajo con respecto al anterior
+	;En caso de que sea >= 615 cambio el de abajo con respecto al anterior
+	;En caso de que sea >= 820 cambio todo
+	pand xmm0, xmm9	;Fila de arriba filtrada
+	pand xmm2, xmm10	;Fila de abajo filtrada
+	pand xmm4, xmm10	;Fila de abajo filtrada
+	pand xmm6, xmm9	;Fila de arriba filtrada
+
+	;combino los datos de la fila de arriba
+	por xmm0, xmm6
+	;combino los datos de la fila de arriba
+	por xmm2, xmm4
+
+	mov r15, rbx
+	add r15, r9
+	movdqu [rsi+rbx], xmm0
+	movdqu [rsi+r15], xmm2
+
+
+	add rsp, 8
+	pop rbx
+	pop r15
+	pop r14
 	pop R13
 	pop R12
 	pop RBP
