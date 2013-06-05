5a68c678ee76cb95033a400906922ea0ad06450b
Sabrina <sizcovich@users.noreply.github.com>
2013-06-05 00:52:47 +0000

    tp3

--- tp3-bundle.v1/src/gdt.c
+++ tp3-bundle.v1/src/gdt.c
@@ -117,18 +117,18 @@ gdt_entry gdt[GDT_COUNT] = {
 		(unsigned char)		0x00,			/* base[31:24]  */
 	},
 	[GDT_IDX_AREA_DESC] = (gdt_entry) {
-		(unsigned short)	0xFFFF,			/* limit[0:15]  */
+		(unsigned short)	0x7FFF,			/* limit[0:15]  */
 		(unsigned short)	0x8000,			/* base[0:15]   */
 		(unsigned char)		0x0B,			/* base[23:16]  */
 		(unsigned char)		0x02,			/* type         */
 		(unsigned char)		0x01,			/* s            */
-		(unsigned char)		0x03,			/* dpl          */
+		(unsigned char)		0x00,			/* dpl          */
 		(unsigned char)		0x01,			/* p            */
-		(unsigned char)		0x07,			/* limit[16:19] */
+		(unsigned char)		0x00,			/* limit[16:19] */
 		(unsigned char)		0x00,			/* avl          */
 		(unsigned char)		0x00,			/* l            */
 		(unsigned char)		0x01,			/* db           */
-		(unsigned char)		0x01,			/* g            */
+		(unsigned char)		0x00,			/* g            */
 		(unsigned char)		0x00,			/* base[31:24]  */
 	},
 };
