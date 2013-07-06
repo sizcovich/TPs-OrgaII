34599db568f5ce1debaa62d4836e75c5c8eeafdb
sizcovich <sizcovich@b194edf2-a248-48e4-bed8-4280180ada1a>
2013-07-06 18:58:55 +0000

    cambie scipt

--- tp2/solucion/src/tiempos.sh
+++ tp2/solucion/src/tiempos.sh
@@ -13,17 +13,17 @@ do
 	echo '\nProcesando archivo: ' $file '\n'
 
 	# recortar
-	echo '\n###########################################################'
-	echo '\nRecortar tamaño 100: \n'
-	./rendimiento.py $TESTSIZE recortar $file 100
-	echo '\n###########################################################'
+#	echo '\n###########################################################'
+#	echo '\nRecortar tamaño 100: \n'
+#	./rendimiento.py $TESTSIZE recortar $file 100
+#	echo '\n###########################################################'
 
 
 	# halftone
-	echo '\n###########################################################'
-	echo '\nHalftone: \n'
-	./rendimiento.py $TESTSIZE halftone $file
-	echo '\n###########################################################'
+#	echo '\n###########################################################'
+#	echo '\nHalftone: \n'
+#	./rendimiento.py $TESTSIZE halftone $file
+#	echo '\n###########################################################'
 
 
 	## colorizar
@@ -34,22 +34,22 @@ do
 
 
 	## umbralizar
-	echo '\n###########################################################'
-	echo '\nUmbralizar minimo 64 maximo 128 q 16: \n'
-	./rendimiento.py $TESTSIZE umbralizar $file 64 128 16
-	echo '\n###########################################################'
+#echo '\n###########################################################'
+#	echo '\nUmbralizar minimo 64 maximo 128 q 16: \n'
+#	./rendimiento.py $TESTSIZE umbralizar $file 64 128 16
+#	echo '\n###########################################################'
+
+
+#	## waves
+#	echo '\n###########################################################'
+#	echo '\nWaves x_scale 16 y_scale 8 g_scale 15: \n'
+#	./rendimiento.py $TESTSIZE waves $file 16 8 15
+#	echo '\n###########################################################'
 
 
 	## waves
-	echo '\n###########################################################'
-	echo '\nWaves x_scale 16 y_scale 8 g_scale 15: \n'
-	./rendimiento.py $TESTSIZE waves $file 16 8 15
-	echo '\n###########################################################'
-
-
-	## waves
-	echo '\n###########################################################'
-	echo '\nRotar: \n'
-	./rendimiento.py $TESTSIZE rotar $file
-	echo '\n###########################################################'
+#	echo '\n###########################################################'
+#	echo '\nRotar: \n'
+#	./rendimiento.py $TESTSIZE rotar $file
+#	echo '\n###########################################################'
 done < $TESTINFILE
